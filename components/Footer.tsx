ca3d2bf761c4efad8a9dac59f634cb446e0558e3
Takumi <takumi.minohara08@gmail.com>
2025-04-23 05:08:53 -0600

    add: footer line

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,50 +5,53 @@ import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () :React.JSX.Element => {
     return (
-        <footer className={styles.footer}>
-            <Image
-                src="https://picsum.photos/400/200"
-                width={400}
-                height={200}
-                alt="footer"
-                style={{
-                    maxWidth: "100%",
-                    height: "auto"
-                }} />
-            <div className={styles.footer_content}>
-                <h2>Let&#39;s make your brand shine!</h2>
-                <div className={styles.footer_menu}>
-                    <div className={styles.footer_link}>
-                        <ul>
-                            <li>
-                                <Link href="/about" >About</Link>
-                            </li>
-                            <li>
-                                <Link href="/services" >Services</Link>
-                            </li>
-                            <li>
-                                <Link href="/portfolio" >Portfolio</Link>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <Link href="/program" >1:1 Program</Link>
-                            </li>
-                            <li>
-                                <Link href="/blog" >Blog</Link>
-                            </li>
-                            <li>
-                                <Link href="/contact" >Contact</Link>
-                            </li>
-                        </ul>
+        <footer className={styles.footer}> 
+            <span className={styles.footer_line}></span>
+            <div className={styles.footer_background}>
+                <Image
+                    src="https://picsum.photos/400/200"
+                    width={400}
+                    height={200}
+                    alt="footer"
+                    style={{
+                        maxWidth: "100%",
+                        height: "auto"
+                    }} />
+                <div className={styles.footer_content}>
+                    <h2>Let&#39;s make your brand shine!</h2>
+                    <div className={styles.footer_menu}>
+                        <div className={styles.footer_link}>
+                            <ul>
+                                <li>
+                                    <Link href="/about" >About</Link>
+                                </li>
+                                <li>
+                                    <Link href="/services" >Services</Link>
+                                </li>
+                                <li>
+                                    <Link href="/portfolio" >Portfolio</Link>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <Link href="/program" >1:1 Program</Link>
+                                </li>
+                                <li>
+                                    <Link href="/blog" >Blog</Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact" >Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.footer_social}>
+                            <ul>
+                            <li><a href="#"><FaFacebookF /></a></li>
+                            <li><a href="#"><FaTwitter /></a></li>
+                            <li><a href="#"><FaInstagram /></a></li>
+                            </ul>
+                        </div>                
                     </div>
-                    <div className={styles.footer_social}>
-                        <ul>
-                        <li><a href="#"><FaFacebookF /></a></li>
-                        <li><a href="#"><FaTwitter /></a></li>
-                        <li><a href="#"><FaInstagram /></a></li>
-                        </ul>
-                    </div>                
                 </div>
             </div>
         </footer>
