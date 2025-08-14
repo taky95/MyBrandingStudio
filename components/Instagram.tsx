dbf476e8068373d82327eb306132d2aee25b3798
Takumi <takumi.minohara08@gmail.com>
2025-08-14 09:04:19 -0600

    fix: Instagram link behaviour

--- components/Instagram.tsx
+++ components/Instagram.tsx
@@ -22,7 +22,7 @@ const Instagram: React.FC<InstagramProps> = ({ instagram }) => {
         <div className={styles.slider_container}>
           {instagram.map((data, index) => (
             <div key={index} className={styles.slider_item}>
-              <Link href={data.permalink} target="_blank" rel="noopener noreferrer">
+              <Link href={data.permalink+'?utm_source=ig_web_copy_link'} target="_blank" rel="noopener noreferrer">
                 <Image src={data.media_url} alt={data.caption} width={250} height={250} unoptimized />
               </Link>
             </div>
